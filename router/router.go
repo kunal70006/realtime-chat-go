56417641be98f6297455a853baa06c011ee38256
Kunaldeep singh <singhkunaldeep@gmail.com>
2023-09-23 01:30:37 +0530

    added cors
    Signed-off-by: Kunaldeep singh <singhkunaldeep@gmail.com>

--- router/router.go
+++ router/router.go
@@ -1,6 +1,9 @@
 package router
 
 import (
+	"time"
+
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/kunal70006/realtime-chat-go/internal/user"
 	"github.com/kunal70006/realtime-chat-go/internal/ws"
@@ -10,6 +13,15 @@ var r *gin.Engine
 
 func InitRouter(userHandler *user.Handler, wsHandler *ws.Handler) {
 	r = gin.Default()
+
+	r.Use(cors.New(cors.Config{
+		AllowAllOrigins:  true,
+		AllowMethods:     []string{"GET", "POST"},
+		AllowHeaders:     []string{"Content-Type"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		MaxAge:           12 * time.Hour,
+	}))
 
 	r.POST("/signup", userHandler.CreateUser)
 	r.POST("login", userHandler.Login)
